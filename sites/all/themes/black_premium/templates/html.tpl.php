21d5e6400325ec8531a03f69da3058d5e73099b7
Erica Wright and Michaela Davis <erica@>
2017-04-24 10:36:44 -0700

    create pet custom module

---
+++ sites/all/themes/black_premium/templates/html.tpl.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+<?php print $head; ?>
+<title><?php print $head_title; ?></title>
+<?php print $styles; ?>
+<?php print $scripts; ?>
+<link href="https://fonts.googleapis.com/css?family=Architects+Daughter" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
